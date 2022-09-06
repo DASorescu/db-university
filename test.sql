bd5af2e7251dfa7e621192af372f8b2dbf2755fb
github_DASorescu <srsdln97@gmail.com>
2022-09-06 17:38:38 +0200

    ASIGNMENT-COMPLETED

--- test.sql
+++ test.sql
@@ -30,12 +30,15 @@ SELECT COUNT(`id`) FROM `teachers` WHERE `phone` IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT YEAR(`enrolment_date`) AS `anno` , COUNT(`id`) FROM `students` GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT `office_address` , COUNT(*) FROM `teachers` GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
-
+SELECT `exam_id` , ROUND(AVG(`vote`),2) AS `media_voti` FROM `exam_student` GROUP BY `exam_id`; --MEDIA VOTI ESAMI PER ID APPELLO 
+-----------------------------EXTRA-------------------------------------------------------------------------------------------------
+SELECT ROUND(AVG(`vote`),2) AS `media_voti` FROM `exam_student` WHERE `exam_id` = 1; --MEDIA VOTI DI UN DATO APPELLO D'ESAME 
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+SELECT `department_id`, COUNT(*) FROM `degrees` GROUP BY `department_id`;

946790af585f226c32e2c42e6b72b6f1220d3dc4
github_DASorescu <srsdln97@gmail.com>
2022-09-06 16:52:46 +0200

    answer-8

--- test.sql
+++ test.sql
@@ -23,7 +23,7 @@ SELECT * FROM `degrees` WHERE `level` LIKE 'magistrale%';
 SELECT `name` FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-
+SELECT * FROM `teachers` WHERE `phone` IS NULL;
 
 
 -- ------------------------------------------ QUERY CON GROUP BY

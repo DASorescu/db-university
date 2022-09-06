f4e42ec9d359c0e89fa0c408fb75f66b93c67f23
github_DASorescu <srsdln97@gmail.com>
2022-09-06 16:45:51 +0200

    answ-5

--- test.sql
+++ test.sql
@@ -14,7 +14,7 @@ SELECT `name`,`date_of_birth`, TIMESTAMPDIFF(YEAR,`date_of_birth`,CURRENT_DATE()
 SELECT * FROM `courses` WHERE `period`LIKE 'I semestre%' AND `year` = 1;
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
-
+SELECT * FROM `exams` WHERE HOUR(`hour`) > 13 AND DATE(`date`) LIKE '2020-06-20%';
 
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 
